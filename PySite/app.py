f9f396d64ded28f16d42d0554ee51b7d60a5cca4
kyrylo Polyakov <kyrylopolyakov@gmail.com>
2023-02-24 18:13:08 +0100

    Flask back-end

---
+++ PySite/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+	return render_template("index.html")
+	
+if __name__ == '__name__':
+	app.run(debug=true , port=8000)
+	
+
